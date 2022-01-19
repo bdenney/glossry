2cee9310da7bff2acacd5f8291098e8735b0ca1d
Brandon Denney <ee.brandon@gmail.com>
2022-01-19 15:22:08 -0500

    Add New Resource form now operational.

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,10 +1,20 @@
 class ResourcesController < ApplicationController
+  protect_from_forgery with: :null_session
 
   def index
     render json: Resource.all
   end
 
   def create
+
+    Resource.create(link: params[:url], title: params[:title], description: params[:description], tags: params[:tags])
+    render json: [], status: :created
+  end
+
+  private
+
+  def create_params
+    params.permit(:url, :title, :description, :tags)
   end
 
 end
--- client/src/ResourceForm.js
+++ client/src/ResourceForm.js
@@ -1,10 +1,33 @@
 import React from "react"
 
 function ResourceForm() {
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        let form = e.target
+        const resourceValues = {
+            url: form.url.value,
+            title: form.title.value,
+            description: form.description.value,
+            tags: form.tags.value
+        }
+
+        fetch('/resources', {
+            method: "POST",
+            headers: {
+                "Content-Type" : "application/json"
+            },
+            body: JSON.stringify(resourceValues)
+        })
+        .then(response => response.json())
+        .then(json => {console.log(json)})
+    }
+
     return (
         <div>
             <h1>Add New Resource</h1>
-            <form>
+            <form onSubmit={handleSubmit} >
                 <label for="url">Url</label>
                 <input id="url" type="text"></input>
 
@@ -12,10 +35,10 @@ function ResourceForm() {
                 <input id="title" type="text"></input>
 
                 <label for="description">Description</label>
-                <input id="description" type=""></input>
+                <input id="description" type="textarea"></input>
 
                 <label for="tags">Tags</label>
-                <input id="tags" type="textarea"></input>
+                <input id="tags" type="text"></input>
 
                 <input type="submit" value="Add"></input>
             </form>
